d6f0584d5a80658a401a1335d111ded15b4a2a47
Farruggia27 <33748156+Farruggia27@users.noreply.github.com>
2017-12-11 21:00:40 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,25 +5,23 @@
 	</head>
 	<body>
 	<?php
-		$n=50;
+		$n=30;
 		$citta="bergamo";
 		$richiesta="pizzeria";
-		# questo script chiama un'API e la inserisce in una tabella 
-		# Indirizzo dell'API da richiedere
-		$fourpage="https://api.foursquare.com/v2/venues/search?v=20161016&query=$richiesta&nit=$n&intent=checkin&client_id=X1GECFVKCU11GJRVBGXCRU0EYKOLTQCLDIIC4RPN2GN2ESLE&client_secret=Y2AXOQKNIL1BJJ1UWUDNLXOTME0D3310Y0XLQDP52HUEZPS5&near=$citta";
-		# Codice di utilizzo di cURL
-		# chiama l'API e la immagazzina in $json
 		
-		$chiamata = curl_init() or die(curl_error());
-		curl_setopt($chiamata, CURLOPT_URL,$fourpage);
-		curl_setopt($chiamata, CURLOPT_RETURNTRANSFER, 1);
-		$json=curl_exec($chiamata) or die(curl_error());
-		# Decodifico la stringa json e la salvo nella variabile $data
-		$jcode = json_decode($json);
+		# indirizzo foursquare con proprie credenziali
+		$fourpage="https://api.foursquare.com/v2/venues/search?v=20161016&query=$richiesta&nit=$n&intent=checkin&client_id=X1GECFVKCU11GJRVBGXCRU0EYKOLTQCLDIIC4RPN2GN2ESLE&client_secret=Y2AXOQKNIL1BJJ1UWUDNLXOTME0D3310Y0XLQDP52HUEZPS5&near=$citta";
+		
+		$memorizza = curl_init() or die(curl_error());	# Memorizza l'api nella variabile $json
+		curl_setopt($memorizza, CURLOPT_URL,$fourpage);
+		curl_setopt($memorizza, CURLOPT_RETURNTRANSFER, 1);
+		$json=curl_exec($memorizza) or die(curl_error());
+		
+		$jcode = json_decode($json); # Decodifico e salvo nella variabile $jcode
 		
 		echo "<table>";
 			echo "<tr>";
-				echo "<th>Nome Pizzeria</th>";
+				echo "<th>N.</th>";
 				echo "<th>Nome Pizzeria</th>";
 				echo "<th>Latitudine</th>";
 				echo "<th>Longitudine</th>";
@@ -46,8 +44,8 @@
 				echo "</tr>";
 			}
 		echo "</table>";
-		echo curl_error($chiamata);
-		curl_close($chiamata);
+		echo curl_error($memorizza);
+		curl_close($memorizza);
 	?>
 	</body>
 </html>
