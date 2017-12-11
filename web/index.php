8bafae2e1f9e4db3a52d349d9be9b97d8b41db23
Farruggia27 <33748156+Farruggia27@users.noreply.github.com>
2017-12-11 21:57:07 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,48 +4,48 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-	   <?php
-		$n=30;
-		$citta="bergamo";
-		$richiesta="pizzeria";
+		<?php
+			$n=30;
+			$citta="bergamo";
+			$richiesta="pizzeria";
 		
-		# indirizzo foursquare con proprie credenziali
-		$fourpage="https://api.foursquare.com/v2/venues/search?v=20161016&query=$richiesta&nit=$n&intent=checkin&client_id=X1GECFVKCU11GJRVBGXCRU0EYKOLTQCLDIIC4RPN2GN2ESLE&client_secret=Y2AXOQKNIL1BJJ1UWUDNLXOTME0D3310Y0XLQDP52HUEZPS5&near=$citta";
+			# indirizzo foursquare con proprie credenziali
+			$fourpage="https://api.foursquare.com/v2/venues/search?v=20161016&query=$richiesta&nit=$n&intent=checkin&client_id=X1GECFVKCU11GJRVBGXCRU0EYKOLTQCLDIIC4RPN2GN2ESLE&client_secret=Y2AXOQKNIL1BJJ1UWUDNLXOTME0D3310Y0XLQDP52HUEZPS5&near=$citta";
 		
-		$memorizza = curl_init() or die(curl_error());	# Memorizza l'api nella variabile $json
-		curl_setopt($memorizza, CURLOPT_URL,$fourpage);
-		curl_setopt($memorizza, CURLOPT_RETURNTRANSFER, 1);
-		$json=curl_exec($memorizza) or die(curl_error());
+			$memorizza = curl_init() or die(curl_error());	# Memorizza l'api nella variabile $json
+			curl_setopt($memorizza, CURLOPT_URL,$fourpage);
+			curl_setopt($memorizza, CURLOPT_RETURNTRANSFER, 1);
+			$json=curl_exec($memorizza) or die(curl_error());
 		
-		$jcode = json_decode($json); # Decodifico e salvo nella variabile $jcode
+			$jcode = json_decode($json); # Decodifico e salvo nella variabile $jcode
 		
-		echo "<table>";
-			echo "<tr>";
-				echo "<th>N.</th>";
-				echo "<th>Nome Pizzeria</th>";
-				echo "<th>Latitudine</th>";
-				echo "<th>Longitudine</th>";
-			echo "</tr>";
-			for($i=0; $i<$n; $i++)
-			{	
+			echo "<table>";
 				echo "<tr>";
-			 		echo "<td>";
-					echo $i+1;
-					echo "</td>";
-					echo "<td>";
-					echo $jcode->response->venues[$i]->name;
-					echo "</td>";
-					echo "<td>";
-					echo $jcode->response->venues[$i]->location->lat;
-					echo "</td>";
-					echo "<td>";
-					echo $jcode->response->venues[$i]->location->lng;
-					echo "</td>";
+					echo "<th>N.</th>";
+					echo "<th>Nome Pizzeria</th>";
+					echo "<th>Latitudine</th>";
+					echo "<th>Longitudine</th>";
 				echo "</tr>";
-			}
-		echo "</table>";
-		echo curl_error($memorizza);
-		curl_close($memorizza);
-	?>
+				for($i=0; $i<$n; $i++)
+				{	
+					echo "<tr>";
+			 			echo "<td>";
+							echo $i+1;
+						echo "</td>";
+						echo "<td>";
+							echo $jcode->response->venues[$i]->name;
+						echo "</td>";
+						echo "<td>";
+							echo $jcode->response->venues[$i]->location->lat;
+						echo "</td>";
+						echo "<td>";
+							echo $jcode->response->venues[$i]->location->lng;
+						echo "</td>";
+					echo "</tr>";
+				}
+			echo "</table>";
+			echo curl_error($memorizza);
+			curl_close($memorizza);
+		?>
 	</body>
 </html>
